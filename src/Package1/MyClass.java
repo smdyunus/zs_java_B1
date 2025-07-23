6b769a996a3c010d38e9439788f9bac1bb0eb67d
yunus <yunus.sqa@gmail.com>
2025-07-23 10:05:05 +0530

    added file

---
+++ src/Package1/MyClass.java
@@ -0,0 +1,24 @@
+package Package1;
+
+class MyClass {
+	//class body
+
+	static int a= 10;
+	static int b = 20;
+	static int z = a+b;
+	
+	String name = "India";
+	
+	public static void main(String[] args) {
+
+		System.out.println("Welcome");
+		System.out.println("TO ZS");
+		System.out.println(z);
+		
+		MyClass OBJ = new MyClass();
+		
+		System.out.println(OBJ.name);
+
+	}
+
+}
